11bc8c18a7562d1102a0c06edfc07194aac0ff61
Carlos <carlosj.corprez@gmail.com>
2024-08-20 19:02:26 -0400

    Created NodeJS server using Express

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const { createServer } = require('node:http');
+// Creating NodeJS Server
 
-const hostname = '127.0.0.1';
+// const { createServer } = require('node:http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World!');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+
+// });
+
+
+// ********************************************************************************//
+// Creating NodeJS Server Using Express
+const express = require('express');
+
+const app = express();
+
 const port = 3000;
 
-const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
 
 });
